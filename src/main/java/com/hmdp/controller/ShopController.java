fed0f9f4aea981e0ec406e584e11e4c3489dcf3d
NULL-Y <1335208529@qq.com>
2024-02-20 20:45:18 +0800

    对于缓存方案优化，增加缓存穿透的处理方案

--- src/main/java/com/hmdp/controller/ShopController.java
+++ src/main/java/com/hmdp/controller/ShopController.java
@@ -31,9 +31,11 @@ public class ShopController {
      * @param id 商铺id
      * @return 商铺详情数据
      */
+    //http://localhost:8080/api/shop/1 这种形式的请求，不是key：value，直接跟在路径后面
     @GetMapping("/{id}")
     public Result queryShopById(@PathVariable("id") Long id) {
-        return Result.ok(shopService.getById(id));
+
+        return shopService.queryById(id);
     }
 
     /**
@@ -57,8 +59,7 @@ public class ShopController {
     @PutMapping
     public Result updateShop(@RequestBody Shop shop) {
         // 写入数据库
-        shopService.updateById(shop);
-        return Result.ok();
+        return shopService.update(shop);
     }
 
     /**
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import io.netty.util.internal.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -16,5 +26,51 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
+    @Override
+    public Result queryById(Long id) {
+        //1. Redis 查询缓存
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        String val = redisTemplate.opsForValue().get(key);
+        //命中直接返回
+        if(StringUtils.isNotBlank(val)) {
+            //转换：
+            Shop shop = JSONUtil.toBean(val, Shop.class);
+            return Result.ok(shop);
+        }
+        if(val != null) {
+            return Result.fail("店铺不存在");
+        }
+        //不命中需要查询数据库：
+        Shop shop = getById(id);
+        if(shop == null) {
+            //不存在返回404
+            redisTemplate.opsForValue().set(key,"", 2L, TimeUnit.MINUTES);
+            return Result.fail("店铺不存在");
+        }
+        //存在：
+        val = JSONUtil.toJsonStr(shop);
+        //写会Redis中，然后返回商铺信息
+        redisTemplate.opsForValue().set(key, val, 30L, TimeUnit.MINUTES);
+        return Result.ok(shop);
+    }
+
+    //保证原子性操作：
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        //先更新再删除：
+        Long id = shop.getId();
+        if(id == null) {
+            return Result.fail("ID不能为空");
+        }
+        //更新：
+        updateById(shop);
+        //删除缓存：
+        redisTemplate.delete(RedisConstants.CACHE_SHOP_KEY + id);
+        return Result.ok();
+
+    }
 }
